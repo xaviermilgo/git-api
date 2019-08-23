b48bd68dc26a6019ab64bd02d855fd5587b84d3b
manuel.bit <emmanuel_njuguna@outlook.com>
2019-08-23 16:10:17 +0300

    add an emit event to the form

--- src/app/search-form/search-form.component.ts
+++ src/app/search-form/search-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-search-form',
@@ -7,7 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchFormComponent implements OnInit {
 
+  @Output()emitSearch = new EventEmitter<any>()
+
+  searchUser:string;
   constructor() { }
+
+  search(){
+    this.emitSearch.emit(this.searchUser);
+  }
 
   ngOnInit() {
   }
